6b59de3f6584324d12df04b22897a9af948d0f98
Alexander Mot <asd777101@gmail.com>
2022-03-19 16:45:48 -0700

    Switch to Fabric

--- README.md
+++
@@ -1,2 +0,0 @@
-# End Shards
-A mod to expand the armour/weapons/tools progression to the end. This new set will be equal to netherrite, but different.
--- src/main/java/dev/amot/endshards/EndShards.java
+++ src/main/java/dev/amot/endshards/EndShards.java
@@ -1,4 +1,21 @@
 package dev.amot.endshards;
 
-public class EndShards {
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndShards implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("endshards");
+	public static final Item endshard = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+
+	@Override
+	public void onInitialize() {
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "endshard"), endshard);
+		LOGGER.info("Mod loaded!");
+	}
 }
