a0bb3bc57dc31e2eff991a4b1a068a9001240e86
Alexander Mot <asd777101@gmail.com>
2022-03-20 17:25:55 -0700

    Add Ender Ingot and recipe

--- src/main/java/dev/amot/endshards/EndShards.java
+++ src/main/java/dev/amot/endshards/EndShards.java
@@ -11,11 +11,17 @@ import org.slf4j.LoggerFactory;
 
 public class EndShards implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("endshards");
+
+	//Create items
 	public static final Item endshard = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+	public static final Item ender_ingot = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
 
 	@Override
 	public void onInitialize() {
+		//Register items
 		Registry.register(Registry.ITEM, new Identifier("endshards", "endshard"), endshard);
+		Registry.register(Registry.ITEM, new Identifier("endshards", "ender_ingot"), ender_ingot);
+
 		LOGGER.info("Mod loaded!");
 	}
 }
