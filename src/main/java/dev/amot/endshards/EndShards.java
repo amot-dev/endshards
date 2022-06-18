6e2daa370e95b7734afd0cb2b860d74c68d49c7c
Alexander Mot <alexander@amot.dev>
2022-06-18 15:35:32 -0700

    Add Warding Heart loot table and texture

--- src/main/java/dev/amot/endshards/EndShards.java
+++ src/main/java/dev/amot/endshards/EndShards.java
@@ -6,6 +6,7 @@ import dev.amot.endshards.features.EndShardsFeatures;
 import dev.amot.endshards.items.EndShardsItems;
 import dev.amot.endshards.items.EnderGear;
 import dev.amot.endshards.items.NetheriteGear;
+import dev.amot.endshards.util.EndShardsLootTables;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +23,7 @@ public class EndShards implements ModInitializer {
 		EndShardsFeatures.register();
 		EnderGear.register();
 		NetheriteGear.register();
+		EndShardsLootTables.init();
 		LOGGER.info("Mod loaded!");
 	}
 }
