fd3818dce546c8308b3270cd29ee79886f3d2a30
Alexander Mot <alexander@amot.dev>
2024-02-24 00:41:20 -0800

    Make thrall projectiles ignore owner and friendly thralls
    Closes #54
    Also clean up LostThrallMixin

---
+++ src/main/java/dev/amot/endshards/mixin/ThrallProjectileCollisionMixin.java
@@ -0,0 +1,33 @@
+package dev.amot.endshards.mixin;
+
+import dev.amot.endshards.util.IThrall;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ProjectileEntity.class)
+public class ThrallProjectileCollisionMixin {
+    @Shadow @Nullable private Entity owner;
+
+    @Inject(method = "canHit", at = @At(value = "RETURN"), cancellable = true)
+    public void canHitThrall(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        // Check if owner of projectile is a thrall
+        if (this.owner != null && ((IThrall)this.owner).isThrall()) {
+            IThrall shootingThrall = ((IThrall)this.owner);
+            // If entity being shot is thrall owner, projectile should not hit
+            if (entity.getUuid() == shootingThrall.getThrallOwnerUUID()) cir.setReturnValue(false);
+            // If entity being shot is a thrall...
+            else if (entity instanceof MobEntity && ((IThrall)entity).isThrall()) {
+                IThrall shotThrall = ((IThrall)entity);
+                // ...and both thralls have the same owner, projectile should not hit
+                if (shotThrall.getThrallOwnerUUID() == shootingThrall.getThrallOwnerUUID()) cir.setReturnValue(false);
+            }
+        }
+    }
+}
