4344883a6a23ba52d43fcfed1d1b63dcf404d3e1
Alexander Mot <alexander@amot.dev>
2025-02-12 22:17:57 -0800

    Improve Strange Crystal drop behaviour Fix #65

--- src/main/java/dev/amot/endshards/blocks/StrangeCrystal.java
+++ src/main/java/dev/amot/endshards/blocks/StrangeCrystal.java
@@ -11,60 +11,65 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
-
 public class StrangeCrystal extends AmethystClusterBlock {
-    private static final int warpRange = 16;
-    private static final int warpAttempts = 16;
-    private static final int height = 7;
-    private static final int offset = 3;
+    private static final int WARP_RANGE = 16;
+    private static final int WARP_ATTEMPTS = 16;
+    private static final int HEIGHT = 7;
+    private static final int OFFSET = 3;
 
     public StrangeCrystal(AbstractBlock.Settings settings) {
-        super(height, offset, settings);
+        super(HEIGHT, OFFSET, settings);
     }
 
     @Override
     public void afterBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, @Nullable BlockEntity blockEntity, ItemStack stack) {
-        player.incrementStat(Stats.MINED.getOrCreateStat(this));
-        player.addExhaustion(0.005f);
-
-        Random random = new Random();
-        boolean blockDropped = false;
-        for (int i = 0; i < warpAttempts; ++i) {
+        // Try teleport drop WARP_ATTEMPTS times
+        boolean successfulTeleport = false;
+        for (int i = 0; i < WARP_ATTEMPTS; ++i) {
+            // Pick random position within WARP_RANGE and world bounds
             BlockPos randomPos = BlockPos.ofFloored(
-                    pos.getX() + (random.nextDouble() - 0.5) * warpRange,
-                    MathHelper.clamp(pos.getY() + (random.nextDouble() - 0.5) * warpRange, world.getBottomY(), world.getTopYInclusive()),
-                    pos.getZ() + (random.nextDouble() - 0.5) * warpRange
+                    pos.getX() + (world.getRandom().nextDouble() - 0.5) * WARP_RANGE,
+                    MathHelper.clamp(pos.getY() + (world.getRandom().nextDouble() - 0.5) * WARP_RANGE, world.getBottomY(), world.getTopYInclusive()),
+                    pos.getZ() + (world.getRandom().nextDouble() - 0.5) * WARP_RANGE
             );
-            if (world.getBlockState(randomPos).getBlock() == Blocks.AIR){
-                if (world.getBlockState(randomPos.offset(Direction.DOWN)).getBlock() != Blocks.AIR){
-                    super.afterBreak(world, player, randomPos, state, blockEntity, stack);
 
-                    // Play teleport sound upon break unless broken by Ender tool (which would cause it to teleport into inventory directly)
-                    Item itemInHand = player.getEquippedStack(EquipmentSlot.MAINHAND).getItem();
-                    if (!(itemInHand instanceof MiningToolItem toolInHand && ((IMiningToolMaterial)toolInHand).endshards$getMaterial() == EnderGear.ENDER_TOOL_MATERIAL)) {
-                        world.playSound(
-                                null,
-                                randomPos.getX(),
-                                randomPos.getY(),
-                                randomPos.getZ(),
-                                SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT,
-                                SoundCategory.BLOCKS,
-                                1.0f,
-                                1.0f
-                        );
-                    }
-                    blockDropped = true;
+            // Ensure position picked is air
+            if (world.getBlockState(randomPos).getBlock() == Blocks.AIR){
+                // Scan downward to land on solid ground
+                while (randomPos.getY() > world.getBottomY() && world.getBlockState(randomPos.offset(Direction.DOWN)).getBlock() == Blocks.AIR) {
+                    randomPos = randomPos.down();
                 }
+                // Break block
+                super.afterBreak(world, player, randomPos, state, blockEntity, stack);
+
+                // Play teleport sound upon break unless broken by Ender tool (which would cause it to teleport into inventory directly)
+                Item itemInHand = player.getEquippedStack(EquipmentSlot.MAINHAND).getItem();
+                if (!(itemInHand instanceof MiningToolItem toolInHand && ((IMiningToolMaterial)toolInHand).endshards$getMaterial() == EnderGear.ENDER_TOOL_MATERIAL)) {
+                    world.playSound(
+                            null,
+                            randomPos.getX(),
+                            randomPos.getY(),
+                            randomPos.getZ(),
+                            SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT,
+                            SoundCategory.BLOCKS,
+                            1.0f,
+                            1.0f
+                    );
+                }
+
+                // Exit loop
+                successfulTeleport = true;
+                break;
             }
         }
-        if (!blockDropped) super.afterBreak(world, player, pos, state, blockEntity, stack);
+
+        // If no successful teleport, drop block at original position
+        if (!successfulTeleport) super.afterBreak(world, player, pos, state, blockEntity, stack);
     }
 }
